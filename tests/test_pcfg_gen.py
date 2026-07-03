df6c62f7e7a0e934effece6d5900407e57e05ae1
Abhishek Rao <abhishekrao5002@gmail.com>
2026-07-03 09:15:07 -0700

    Add test for separate grammar and interpreter.

--- tests/test_pcfg_gen.py
+++ tests/test_pcfg_gen.py
@@ -1,9 +1,17 @@
-from induce_text import pcfg_gen
+from induce_text.pcfg_gen import BinaryHexChoice, Rule, DataGenerator
 
 
-def test_generator_sample_produces_data():
-    gen = pcfg_gen.DataGenerator(seed=42)
-    data = gen.sample()
-    assert isinstance(data, pcfg_gen.HexList)
-    assert data.data
-    assert all(isinstance(x, pcfg_gen.Hex) for x in data.data)
+def test_generator_sample_produces_different_data():
+    rule = Rule(
+        symbols=[0x0, 0x0, BinaryHexChoice(a=1, b=2), 0x0, BinaryHexChoice(a=3, b=4)]
+    )
+    all_data = []
+    for seed in range(100):
+        gen = DataGenerator(seed=seed, rules=[rule])
+        data = gen.sample()
+        print(data)
+        data = gen.sample()
+        assert isinstance(data, list)
+        assert all(isinstance(x, int) for x in data)
+        all_data.append(tuple(data))
+    assert len(set(all_data)) == 4
